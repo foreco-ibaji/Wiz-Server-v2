051a63bdeaca749da80efa6f0a10b78c919b2872
D0ri123 <lsh81550@gmail.com>
2023-06-01 18:22:47 +0900

    setting: 초기 세팅

--- src/test/java/com/sesacthon/foreco/ForecoApplicationTests.java
+++ src/test/java/com/sesacthon/foreco/ForecoApplicationTests.java
@@ -3,6 +3,10 @@ package com.sesacthon.foreco;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * test코드의 application.yml 파일이 없어서 test 빌드가 실패함
+ * @SpringBootTest 를 주석처리함
+ */
 //@SpringBootTest
 class ForecoApplicationTests {
 
