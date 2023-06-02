f248c30408a95966844db185ae3abff82aa671a7
D0ri123 <lsh81550@gmail.com>
2023-06-02 16:39:43 +0900

    setting: 테스트 주석처리 취소

--- src/test/java/com/sesacthon/foreco/ForecoApplicationTests.java
+++ src/test/java/com/sesacthon/foreco/ForecoApplicationTests.java
@@ -3,11 +3,7 @@ package com.sesacthon.foreco;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * test코드의 application.yml 파일이 없어서 test 빌드가 실패함
- * @SpringBootTest 를 주석처리함
- */
-//@SpringBootTest
+@SpringBootTest
 class ForecoApplicationTests {
 
   @Test
