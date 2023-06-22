cd06fcc144ae1664cf7929755229b69363b224af
D0ri123 <lsh81550@gmail.com>
2023-06-22 17:53:04 +0900

    chore: 공백 제거

--- src/main/java/com/sesacthon/foreco/HealthController.java
+++ src/main/java/com/sesacthon/foreco/HealthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HealthController {
-
   @GetMapping("/health")
   public String healthCheck() {
     return "OK!";
