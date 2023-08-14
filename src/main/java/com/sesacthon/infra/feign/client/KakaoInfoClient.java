63f4cba43b1efbf6cac8046618b1e8dd37248f5d
D0ri123 <lsh81550@gmail.com>
2023-08-14 15:22:28 +0900

    feat: 카카오 로그인 관련 feign 설정

---
+++ src/main/java/com/sesacthon/infra/feign/client/KakaoInfoClient.java
@@ -0,0 +1,21 @@
+package com.sesacthon.infra.feign.client;
+
+import com.sesacthon.infra.feign.config.KakaoFeignConfig;
+import com.sesacthon.infra.feign.dto.response.KakaoUserInfoResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+/**
+ * 카카오 사용자 정보 얻어오는 feign client
+ */
+@FeignClient(name = "kakaoInfoClient", url = "https://kapi.kakao.com", configuration = KakaoFeignConfig.class)
+@Component
+public interface KakaoInfoClient {
+
+  //https://kapi.kakao.com/v2/user/me => 회원 정보 요청 url
+  @GetMapping(value = "/v2/user/me")
+  KakaoUserInfoResponseDto getUserInfo(@RequestHeader(name = "Authorization") String Authorization);
+
+}
