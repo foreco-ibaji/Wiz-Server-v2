271237f5e58c965bd8f0e755e49027adfda736de
D0ri123 <lsh81550@gmail.com>
2023-08-14 19:25:58 +0900

    feat: redis 도입 및 토큰 재발행 서비스 구현

---
+++ src/main/java/com/sesacthon/global/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.sesacthon.global.security;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+import com.sesacthon.foreco.jwt.filter.JwtAuthenticationEntryPointHandler;
+import com.sesacthon.foreco.jwt.filter.JwtAuthenticationFilter;
+import com.sesacthon.foreco.jwt.filter.JwtExceptionFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final JwtExceptionFilter jwtExceptionFilter;
+  private final JwtAuthenticationEntryPointHandler jwtAuthenticationEntryPointHandler;
+  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .sessionManagement(session ->
+            session.sessionCreationPolicy(STATELESS))
+        .authorizeHttpRequests(authorize ->
+            authorize.requestMatchers(HttpMethod.GET, "/api/v1/member").authenticated()
+                     .requestMatchers("/**").permitAll())
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+
+    http.exceptionHandling(exception ->
+        exception.authenticationEntryPoint(jwtAuthenticationEntryPointHandler)
+            .accessDeniedHandler(jwtAccessDeniedHandler));
+
+    return http.build();
+  }
+}
---
+++ src/main/java/com/sesacthon/global/util/CookieUtil.java
@@ -0,0 +1,30 @@
+package com.sesacthon.global.util;
+
+import static com.sesacthon.foreco.jwt.service.JwtTokenProvider.REFRESH_TOKEN_EXPIRE_LENGTH_MS;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+
+
+/**
+ * Cookie 관련 유틸리티 클래스.
+ */
+public class CookieUtil {
+
+  private CookieUtil() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  public static void setRefreshCookie(HttpHeaders httpHeaders, String refreshToken) {
+
+    ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+        .httpOnly(true)
+        .secure(true)
+        .maxAge(REFRESH_TOKEN_EXPIRE_LENGTH_MS)
+        .path("/")
+        .sameSite("None")
+        .build();
+
+    httpHeaders.add(HttpHeaders.SET_COOKIE, cookie.toString());
+  }
+}
