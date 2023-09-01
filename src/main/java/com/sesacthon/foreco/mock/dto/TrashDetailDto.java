7677955a66888a8123d0995324009c9cabb5a125
hoiae <donald819@naver.com>
2023-09-02 01:41:19 +0900

    fix:mock api수정
    쓰레기 상세조회에 필요한 iconUrl추가

--- src/main/java/com/sesacthon/foreco/mock/dto/TrashDetailDto.java
+++ src/main/java/com/sesacthon/foreco/mock/dto/TrashDetailDto.java
@@ -15,14 +15,16 @@ public class TrashDetailDto {
   private final String disposalMethod;
   private final DisposalInfoDto disposalInfoDto; //기존에 있는 DTO 사용!
   private final List<String> remark;
+  private final String iconUrl;
 
   public TrashDetailDto(Long id, DetailType detailType, String name, String disposalMethod,
-      DisposalInfoDto disposalInfoDto, List<String> remark) {
+      DisposalInfoDto disposalInfoDto, List<String> remark, String iconUrl) {
     this.id = id;
     this.detailType = detailType;
     this.name = name;
     this.disposalMethod = disposalMethod;
     this.disposalInfoDto = disposalInfoDto;
     this.remark = remark;
+    this.iconUrl = iconUrl;
   }
 }
