493e4c8860037b568294d9637e56eba882672356
hoiae <donald819@naver.com>
2023-09-06 02:54:53 +0900

    refactor: /api/v1/image api응답 데이터에 id필드 추가
    이미지를 ai모델에 전달하여 응답된 값이 조회가능한 쓰레기인경우 해당쓰레기의 id필드에 고유id를 반환하며, 복합재질이나 조회할 수없는 품목인 경우 id필드에 -1를 반환합니다.

--- src/main/java/com/sesacthon/infra/s3/dto/UploadDto.java
+++ src/main/java/com/sesacthon/infra/s3/dto/UploadDto.java
@@ -7,10 +7,12 @@ public class UploadDto {
 
   private final String message;
   private final String result;
+  private final Long id;
 
-  public UploadDto(String message, String result) {
+  public UploadDto(String message, String result, Long id) {
     this.message = message;
     this.result = (result == null) ? "인식할 수 없음" : result;
+    this.id = id;
   }
 
 }
