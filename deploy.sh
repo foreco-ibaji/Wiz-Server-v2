5afd96b96c0369a5a6d3ef857d20a2289e7f98a6
D0ri123 <lsh81550@gmail.om>
2024-02-07 18:33:23 +0900

    [MRUY-7] github action 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,16 @@ DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
 if [ -z $IS_GREEN  ];then # blue라면
 
-  echo "### BLUE => GREEN ###"
+  echo "### CHANGE BLUE TO GREEN ###"
 
-  echo "1. get green image"
+  echo "1. Pull green"
   docker-compose pull green # green으로 이미지를 내려받습니다.
 
-  echo "2. green container up"
+  echo "2. Green container up"
   docker-compose up -d green # green 컨테이너 실행
 
   while [ 1 = 1 ]; do
-  echo "3. green health check..."
+  echo "3. Green health check..."
   sleep 3
 
   REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
@@ -24,23 +24,23 @@ if [ -z $IS_GREEN  ];then # blue라면
             fi
   done;
 
-  echo "4. reload nginx"
+  echo "4. Reload NGINX"
   sudo cp /nginx/conf.d/nginx.green.conf /etc/nginx/nginx.conf
   sudo nginx -s rel
 
-  echo "5. blue container down"
+  echo "5. Blue container down"
   docker-compose stop blue
 else
-  echo "### GREEN => BLUE ###"
+  echo "### CHANGE GREEN TO BLUE ###"
 
-  echo "1. get blue image"
+  echo "1. Pull BLUE"
   docker-compose pull blue
 
-  echo "2. blue container up"
+  echo "2. Blue container up"
   docker-compose up -d blue
 
   while [ 1 = 1 ]; do
-    echo "3. blue health check..."
+    echo "3. Blue health check..."
     sleep 3
     REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
 
