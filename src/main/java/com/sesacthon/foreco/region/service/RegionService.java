b92e4fd9753334e3544f64281a2091b08b5d8b24
LeeSeolHui <lsh81550@gmail.com>
2024-08-18 20:55:37 +0900

    feature: 매칭되는 지역 데이터가 없더라도 가입이 가능하도록 로직 개선
    Signed-off-by: LeeSeolHui <lsh81550@gmail.com>

--- src/main/java/com/sesacthon/foreco/region/service/RegionService.java
+++ src/main/java/com/sesacthon/foreco/region/service/RegionService.java
@@ -6,6 +6,7 @@ import com.sesacthon.foreco.region.entity.Region;
 import com.sesacthon.foreco.region.exception.RegionNotFoundException;
 import com.sesacthon.foreco.region.repository.RegionRepository;
 import com.sesacthon.global.verification.VerifyRegionValueService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,12 @@ public class RegionService {
 
   public Region findRegion(String region) {
     String[] regionVal = verifyRegionValueService.checkParam(region);
-    return regionRepository.findRegionByCityAndGuAndDong(regionVal[0], regionVal[1], regionVal[2])
-        .orElseThrow(() -> new RegionNotFoundException(REGION_NOT_FOUND));
+
+    Optional<Region> regionInfo = regionRepository.findRegionByCityAndGuAndDong(regionVal[0],
+        regionVal[1], regionVal[2]);
+
+    // 1번에 가장 기본지역 데이터를 넣어둬야 함
+    return regionInfo.orElseGet(() -> findRegion(1L));
   }
 
   //region 정보가 없을 경우
---
+++ src/test/java/com/sesacthon/foreco/region/service/RegionServiceTest.java
@@ -0,0 +1,13 @@
+package com.sesacthon.foreco.region.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegionServiceTest {
+
+    @Test
+    void findRegion_일치하는_지역이_없는_경우() {
+        // TODO: 데이터 채워넣은 후 테스트 코드 작성 필요
+    }
+}
